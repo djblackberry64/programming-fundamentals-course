6cd86cc08c07c5fbf117905326570c7a6996144c
djblackberry64 <220242469+djblackberry64@users.noreply.github.com>
2026-05-21 13:57:20 +0200

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://djblackberry64.github.io/galactic-gamma",
   integrations: [
     starlight({
       title: "My Docs",
