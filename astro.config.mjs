e690735d1882e5f49f759fd886898455eb08d3c2
djblackberry64 <220242469+djblackberry64@users.noreply.github.com>
2026-06-08 15:38:42 +0200

    Change base in config to match new repo link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://djblackberry64.github.io",
-  base: "/galactic-gamma",
+  base: "/programming-fundamentals-course",
   integrations: [
     starlight({
       title: "My Docs",
